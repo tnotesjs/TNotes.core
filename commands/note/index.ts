1f420db0e895f0b04716cc0ddffa2521c123526a
Tdahuyou <dahuyoutop@gmail.com>
2026-02-20 22:27:35 +0800

    feat: init TNotes.core with scripts from TNotes.introduction

---
+++ commands/note/index.ts
@@ -0,0 +1,8 @@
+/**
+ * .vitepress/tnotes/commands/note/index.ts
+ *
+ * Note 命令统一导出
+ */
+export { CreateNoteCommand } from './CreateNoteCommand'
+export { UpdateNoteConfigCommand } from './UpdateNoteConfigCommand'
+export { RenameNoteCommand } from './RenameNoteCommand'
---
+++ services/vitepress/index.ts
@@ -0,0 +1,6 @@
+/**
+ * .vitepress/tnotes/services/vitepress/index.ts
+ *
+ * Vitepress 服务入口
+ */
+export { VitepressService } from './service'
